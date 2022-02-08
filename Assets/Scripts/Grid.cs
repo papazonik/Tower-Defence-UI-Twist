1bc73a9c4ba2440c2ffb951493901f3d7e72c995
Simon Mellkvist <simon.z.mellkvist@hotmail.com>
2022-02-08 19:59:23 +0100

    Some more work on gridsystem

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -6,12 +6,29 @@ public class Grid : MonoBehaviour
 {
     private int width;
     private int height;
+    private float cellSize;
     private int[,] gridArray;
-    public Grid(int width, int height)
+    public Grid(int width, int height, float cellSize)
     {
         this.width = width;
         this.height = height;
+        this.cellSize = cellSize;
 
         gridArray = new int[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++) //Loops lenght in first dimension (width)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++) //Loops lenght in second dimension (height)
+            {
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+    }
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize;
     }
 }
--- Assets/Scripts/GridTest.cs
+++ Assets/Scripts/GridTest.cs
@@ -7,7 +7,7 @@ public class GridTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Grid grid = new Grid(12, 7, 10.0f);
     }
 
     // Update is called once per frame
